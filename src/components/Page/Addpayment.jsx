701f5af77525e728f0451b4df5acba59f5225ad6
lucia nambi <nambilucia@gmail.com>
2025-01-06 15:55:39 +0300

    change add button color

--- src/components/Page/Addpayment.jsx
+++ src/components/Page/Addpayment.jsx
@@ -247,7 +247,7 @@ function Addpayment() {
                         </div>
                     </div>
 
-                    <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300">
+                    <button type="submit" className="w-full bg-[#3F51B5] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300">
                         Add Payment
                     </button>
                 </form>
